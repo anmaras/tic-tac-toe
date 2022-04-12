061942937479b37ca55455b3db51ba6dbb713248
Antonis <marasantonis@gmail.com>
2022-04-12 20:14:22 +0300

    script name update

--- script.js
+++ script.js
@@ -1,100 +1,172 @@
-let array = Array(9).fill(null);
-const playerX = "X";
-const playerO = "O";
-let defaultPlayer = playerX;
-const boardCells = document.querySelectorAll(".grid-item");
-const restart = document.querySelector(".restart-btn");
-const firstPlayerTurn = "Player One Turn";
-const secondPlayerTurn = "Player Two Turn";
-const winCombination = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+/* Array & Array win combo */
+const displayControls = (function () {
+  const cells = document.querySelectorAll(".grid-item");
+  const restart = document.querySelector(".restart-btn");
+  const replay = document.querySelector(".replay-btn");
+  const scoreBoard = document.querySelector(".score");
+  const blackdrob = document.querySelector(".blackdrop");
+  const cellWinBgColor = "grey";
+  const cellWinTextColor = "White";
+  const emptyBoard = Array(9).fill(null);
+  const winBoard = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  return {
+    emptyBoard,
+    winBoard,
+    cellWinBgColor,
+    cellWinTextColor,
+    cells,
+    restart,
+    scoreBoard,
+    blackdrob,
+    replay,
+  };
+})();
 
-const startGame = () => {
-  boardCells.forEach((cell) =>
-    cell.addEventListener("click", addEntriesInToGridCell)
-  );
+const playerFactory = (name, score = 0) => {
+  return { name, score };
 };
 
-function addEntriesInToGridCell() {
-  if (!array[this.id]) {
-    array[this.id] = defaultPlayer;
-    this.textContent = defaultPlayer;
-    /* assign to variable condition to toggle between X and O */
-    defaultPlayer = defaultPlayer == playerX ? playerO : playerX;
+const gamePlayModule = (() => {
+  const player1 = playerFactory("X");
+  const player2 = playerFactory("O");
+  const defaultPlayer = playerFactory(player1.name);
+  let winArray = [];
+
+  /* Public Function */
+  function playerChoice() {
+    if (!displayControls.emptyBoard[this.id] && !this.textContent) {
+      displayControls.emptyBoard[this.id] = defaultPlayer.name;
+      this.textContent = defaultPlayer.name;
+
+      //tenary condition
+      defaultPlayer.name =
+        defaultPlayer.name == player1.name ? player2.name : player1.name;
+    }
+    _winCondition(this.textContent);
   }
-  checkWhoIsTheWinnerr(this.textContent);
 
-  const functionValue = checkWhoIsTheWinnerr(this.textContent);
-
-  /* Change the bg of winning boxes */
-  if (functionValue) {
-    changeWinBoxBg(functionValue);
-    removeEventAfterWin();
-  }
-  checkForDraw();
-}
-
-/* Check win conditions  */
-function checkWhoIsTheWinnerr(symbol) {
-  for (const condition of winCombination) {
-    /* With the use of array destructoring extract all the first index values from winConbination array
-    to a then do the same with the second and then with the third index */
-    let [a, b, c] = condition;
-    /* using the values from deconstructed array as indexes for main array 
-i can check if there is any win condition on the board*/
-    if (array[a] === symbol && array[b] === symbol && array[c] === symbol) {
-      return [a, b, c];
+  /* Private Function */
+  function _winCondition(textContent) {
+    for (const condition of displayControls.winBoard) {
+      const [a, b, c] = condition;
+      if (
+        displayControls.emptyBoard[a] === textContent &&
+        displayControls.emptyBoard[b] === textContent &&
+        displayControls.emptyBoard[c] === textContent
+      ) {
+        winArray = [a, b, c];
+        _removeEventListener();
+        _addPointOnWinCondition(winArray);
+        _changeFontAndBgStyleOnWin(winArray);
+      }
+      if (displayControls.emptyBoard.every((cell) => cell != null)) {
+        displayControls.scoreBoard.textContent = "Draw";
+        return console.log("Draw");
+      }
     }
   }
-}
 
-/* Check for Draw */
-function checkForDraw() {
-  if (array.every((cell) => cell != null)) {
-    return alert("DRAW");
+  function _removeEventListener() {
+    displayControls.cells.forEach((cell) =>
+      cell.removeEventListener("click", gamePlayModule.playerChoice)
+    );
   }
-}
 
-/* BackDrop Toggle When is a Win */
-function playerWinMsg(input) {
-  if (input) {
-    document.body.style.background = "blue";
+  /* Private Function */
+  function _addPointOnWinCondition(array) {
+    if (
+      array.every(
+        (value) => displayControls.cells[value].textContent === player1.name
+      )
+    ) {
+      player1.score++;
+      displayControls.scoreBoard.textContent = `${player1.score} : ${player2.score}`;
+    } else if (
+      array.every(
+        (value) => displayControls.cells[value].textContent === player2.name
+      )
+    ) {
+      player2.score++;
+      displayControls.scoreBoard.textContent = `${player1.score} : ${player2.score}`;
+    }
+    _counterLimit();
   }
-}
 
-/* Change the bg of winning boxes */
-function changeWinBoxBg(input) {
-  const boxArray = input;
-  boxArray.map((box) => (boardCells[box].style.background = "red"));
-}
+  /* Private Function */
+  function _counterLimit() {
+    const maxWins = 2;
+    if (player1.score >= maxWins) {
+      console.log("player1 won");
+      _blackDropAddAndRestartBtn();
+    } else if (player2.score >= maxWins) {
+      console.log("player2 won");
+      _blackDropAddAndRestartBtn();
+    }
+  }
 
-function removeEventAfterWin() {
-  boardCells.forEach((cell) =>
-    cell.removeEventListener("click", addEntriesInToGridCell)
-  );
-}
+  /* Private Function for grid cell bg clr change on win*/
+  function _changeFontAndBgStyleOnWin(winArray) {
+    if (winArray) {
+      winArray.forEach(
+        (value) => (
+          (displayControls.cells[value].style.background =
+            displayControls.cellWinBgColor),
+          (displayControls.cells[value].style.color =
+            displayControls.cellWinTextColor)
+        )
+      );
+    }
+  }
 
-/* Refresh the page  */
-function restartTheGame() {
-  array.fill(null);
-  boardCells.forEach(
-    (cell) => (
-      (cell.textContent = ""),
-      ((cell.style.background = ""),
-      cell.addEventListener("click", addEntriesInToGridCell))
-    )
-  );
-  defaultPlayer = playerX;
-}
+  function _blackDropAddAndRestartBtn() {
+    displayControls.blackdrob.classList.add("visible");
+    displayControls.restart.classList.add("visible");
+  }
 
-restart.addEventListener("click", restartTheGame);
+  function _blackDropAndRestastRemove() {
+    displayControls.blackdrob.classList.remove("visible");
+    displayControls.restart.classList.remove("visible");
+  }
 
-startGame();
+  /* Public Function */
+  function replay() {
+    displayControls.emptyBoard.fill(null);
+    displayControls.cells.forEach(
+      (cell) => (
+        (cell.textContent = ""),
+        (cell.style.background = ""),
+        (cell.style.color = ""),
+        cell.addEventListener("click", gamePlayModule.playerChoice)
+      )
+    );
+    defaultPlayer.name = player1.name;
+    displayControls.scoreBoard.textContent = `${player1.score} : ${player2.score}`;
+  }
+
+  function restart() {
+    replay();
+    _blackDropAndRestastRemove();
+    player1.score = 0;
+    player1.score = 0;
+    displayControls.scoreBoard.textContent = `${player1.score} : ${player2.score}`;
+  }
+
+  return { playerChoice, replay, restart };
+})();
+
+displayControls.cells.forEach((cell) =>
+  cell.addEventListener("click", gamePlayModule.playerChoice)
+);
+
+displayControls.replay.addEventListener("click", gamePlayModule.replay);
+
+displayControls.restart.addEventListener("click", gamePlayModule.restart);
